cb73c5979931d5d2149d2ca36f3b86a1c31491d7
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-11-02 22:07:28 +0100

    add clap
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+extern crate clap;
 extern crate rustc_serialize;
 
 mod data;
 
+use clap::{App,Arg,SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+                        .version(env!("CARGO_PKG_VERSION"))
+                        .author(env!("CARGO_PKG_AUTHORS"))
+                        .about(env!("CARGO_PKG_DESCRIPTION"))
+                        .arg(Arg::with_name("version")
+                                    .short("v")
+                                    .long("version")
+                                    .help("Prints version information"))
+                        .get_matches();
+
+    if matches.is_present("version") {
+        println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    }
 }
