f6a408cce8e874e4fbd98d13ad731f14b3f12ddd
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-09-26 16:10:00 +0200

    simplify prompting and correctly handle passwords
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/add.rs
+++ src/add.rs
@@ -1,15 +1,15 @@
 use clap::{ArgMatches};
-use mnemesis_utils::prompt_for_input;
+use mnemesis_utils::{prompt_for_input, prompt_for_password};
 use serde_json::{self};
 
 use data::Credentials;
 
 pub fn _main(args: &ArgMatches) {
-    let url         = prompt_for_input("Url:");
-    let login       = prompt_for_input("Login:");
-    let password    = prompt_for_input("Password:");
+    let url         = prompt_for_input("Url: ");
+    let login       = prompt_for_input("Login: ");
+    let password    = prompt_for_password("Password: ");
     let totp_secret = if args.is_present("totp") {
-        Some(prompt_for_input("TOTP secret"))
+        Some(prompt_for_input("TOTP secret: "))
     } else {
         None
     };
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use std::io::{self, Write};
+extern crate rpassword;
+extern crate rprompt;
 
 pub fn prompt_for_input(msg: &str) -> String {
-    print!("{} ", msg);
-    io::stdout().flush().expect("Failed flushing stdout");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed reading from stdin");
-    input.trim().to_string()
+    rprompt::prompt_reply_stdout(msg).expect("Failed reading from stdin")
+}
+
+pub fn prompt_for_password(msg: &str) -> String {
+    rpassword::prompt_password_stdout(msg).expect("Failed reading password")
 }
