b621cd2ce70f0ec0759a7f4dddeba82bff604d9f
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-09-26 15:18:33 +0200

    initial subcommands infrastructure
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ src/add.rs
@@ -0,0 +1,5 @@
+use clap::{ArgMatches};
+
+pub fn _main(args: &ArgMatches) {
+    println!("add: {:?}", args);
+}
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,21 @@ extern crate serde;
 #[macro_use] extern crate serde_derive;
 extern crate serde_json;
 
-mod data;
+mod add;
 
-use clap::{App,Arg,SubCommand};
+use clap::{App, SubCommand};
 
 fn main() {
     let matches = App::new(env!("CARGO_PKG_NAME"))
-                        .version(env!("CARGO_PKG_VERSION"))
-                        .author(env!("CARGO_PKG_AUTHORS"))
-                        .about(env!("CARGO_PKG_DESCRIPTION"))
-                        .get_matches();
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(SubCommand::with_name("add").about("Add new credentials to the registry"))
+        .get_matches();
+
+    if let Some(sub) = matches.subcommand_matches("add") {
+        add::_main(sub);
+    } else {
+        println!("{}", matches.usage());
+    }
 }
