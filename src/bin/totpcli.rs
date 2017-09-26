7337abf6ff3cd82f001f348c898a1f6f376fb5e0
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-09-26 10:35:56 +0200

    totpcli: add padding
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/bin/totpcli.rs
+++ src/bin/totpcli.rs
@@ -6,5 +6,5 @@ use oath::{totp_raw_now, HashType};
 use std::env;
 
 fn main() {
-    println!("{:?}", totp_raw_now(base32::decode(base32::Alphabet::RFC4648{ padding: false }, &env::args().nth(1).unwrap()).unwrap().as_ref(), 6, 0, 30, &HashType::SHA1));
+    println!("{:06}", totp_raw_now(base32::decode(base32::Alphabet::RFC4648{ padding: false }, &env::args().nth(1).unwrap()).unwrap().as_ref(), 6, 0, 30, &HashType::SHA1));
 }
